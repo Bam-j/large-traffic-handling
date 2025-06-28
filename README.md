d02b8a401467db915ad0ddaa071e26ec32791c64
bam <juhyun19981129@gmail.com>
2025-06-28 21:46:47 +0900

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# 대규모 트래픽 분산 처리 시스템
+- 대규모 트래픽이 발생하는 상황을 만들고 해당 상황을 처리하는 시스템입니다.
+- 프로젝트 제시 상황: 쇼핑몰에서 하나의 상품에 초 당 수백 ~ 수천 ~ 수만의 트래픽이 몰리는 상황
+- Java 17, SpringBoot, Spring WebFlux, Redis, Apache JMeter, REST API, Thymeleaf
---
+++ src/test/java/com/joo/largetraffichandling/LargetraffichandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joo.largetraffichandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LargetraffichandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
